e9e3fd34f9d5db6b510d692ca375d3623c5442ec
BJTU17221294 <839891341@qq.com>
2020-09-11 11:20:35 +0800

    No560.py

--- leetcode/google/array/No846.py
+++ leetcode/google/array/No846.py
@@ -9,17 +9,18 @@
 import collections
 
 
-class Solution(object):
-    def isNStraightHand(self, hand, W):
-        count = collections.Counter(hand)
-        while count:
-            m = min(count)
-            for k in range(m, m + W):
-                v = count[k]
-                if not v: return False
-                if v == 1:
-                    del count[k]
-                else:
-                    count[k] = v - 1
+def isNStraightHand(hand, W):
+    count = collections.Counter(hand)
+    while count:
+        m = min(count)
+        for k in range(m, m + W):
+            v = count[k]
+            if not v: return False
+            if v == 1:
+                del count[k]
+            else:
+                count[k] = v - 1
 
-        return True
+    return True
+
+
