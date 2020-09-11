62dda331514851f1da25d702590dbda96551f069
BJTU17221294 <839891341@qq.com>
2020-09-11 16:55:20 +0800

    go

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-## questions
+## _questions:_
 
 * _These questions are all come from Algoexpert and solutions are offered by the ricoyang._ 
 * _Link:`https://www.algoexpert.io/product`_
 * _Thanks to them helping me to prepare the algorithm interview._
 * _some questions about google company are also added from Leetcode_
 
-## current Situation:
+## _current Situation:_
 
 _University:JiaoTong University_
 
---
+++ leetcode/google/strings/No127.py
@@ -0,0 +1,23 @@
+# Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation
+# sequence from beginWord to endWord, such that:
+#
+# Only one letter can be changed at a time.
+# Each transformed word must exist in the word list.
+# Note:
+#
+# Return 0 if there is no such transformation sequence.
+# All words have the same length.
+# All words contain only lowercase alphabetic characters.
+# You may assume no duplicates in the word list.
+# You may assume beginWord and endWord are non-empty and are not the same.
+
+def ladderLength(beginWord, endWord, wordList):
+    """
+    :type beginWord: str
+    :type endWord: str
+    :type wordList: List[str]
+    :rtype: int
+    """
+
+
+
