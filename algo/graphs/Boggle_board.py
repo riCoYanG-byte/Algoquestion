a010eb396370fb2c3b2c4d46bbdd9a9cee0f9481
BJTU17221294 <839891341@qq.com>
2020-09-15 11:50:34 +0800

    modify

--- algo/graphs/Boggle_board.py
+++ algo/graphs/Boggle_board.py
@@ -1,9 +1,9 @@
-#build a tree to store a bunch of words is okay
+# build a tree to store a bunch of words is okay
 
 def boggleBoard(board, words):
     # Write your code here.
-    check =  [[None for values in row] for row in board]
+    check = [[None for values in row] for row in board]
     for i in range(len(board)):
         for j in range(len(board[0])):
-
-
+            pass
+    # todo
